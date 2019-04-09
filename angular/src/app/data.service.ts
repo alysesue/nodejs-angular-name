c4174662e20bb05c829f4f9dfd5a717138d472fc
AS <misstechspec@gmail.com>
2019-04-09 21:54:47 +1000

    add service

--- angular/src/app/data.service.ts
+++ angular/src/app/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  httpOptions: { headers; observe; } = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    observe: 'response'
+  };
+
+  registerPerson(person) {
+    return this.http.post('http://localhost:1000/api/v1/add', person, this.httpOptions );
+  }
+
 }
