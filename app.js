bfdd078b018776e45b58ef428c9a6222492076b5
AS <misstechspec@gmail.com>
2019-04-09 18:20:40 +1000

    add api for name

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // set routes to use express app
@@ -21,7 +21,7 @@ personController(app);
 
 // server startup
 app.listen(1000, function () {
-  console.log('App listening on port 1000');
+    console.log('App listening on port 1000');
 })
 
 // export module so can use in other modules
--- controllers/person.controller.js
+++ controllers/person.controller.js
@@ -7,6 +7,51 @@ const { Datastore } = require('@google-cloud/datastore');
 
 const personController = function(app) {
 
+    const datastore = new Datastore({
+        projectId: 'datastorepuppies',
+        keyFilename: 'DataStorePuppies-ce7544c8af76.json'
+    });
+
+    const dataService = new DataService(datastore);
+
+    // add
+    app.post("/api/v1/add", function (req, res) { 
+        let firstName = req.body.firstName;
+        let lastName = req.body.lastName;
+
+        let person = new Person(firstName, lastName)
+
+        let personKey = datastore.key('Person');
+
+        let entity = {
+            key: personKey,
+            data: [
+              {
+                name: 'firstName',
+                value: person.firstName,
+                excludeFromIndexes: false,
+              },
+              {
+                name: 'lastName',
+                value: person.lastName,
+                excludeFromIndexes: false,
+              },
+            ],
+        };
+
+        dataService
+            .create(entity)
+            .then(() => {
+              res.json(
+                firstName + ' ' + lastName + ' is now registered. '
+                )
+            })
+            .catch((err) => {
+              res.json(
+                'Error registering: ' + err
+              )
+            });
+    })
 
 }
 
