0ea72ef1242855c1b4313246c95a8c23bfede55d
AS <misstechspec@gmail.com>
2019-04-09 19:22:06 +1000

    add person route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonComponent } from './person/person.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PersonComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
