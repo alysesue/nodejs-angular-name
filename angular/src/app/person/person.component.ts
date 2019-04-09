e90e8301a2855004c65fef8e6d93c06ef066c3b6
AS <misstechspec@gmail.com>
2019-04-09 21:52:45 +1000

    add html

--- angular/src/app/person/person.component.ts
+++ angular/src/app/person/person.component.ts
@@ -1,41 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.scss']
+  styleUrls: ['./person.component.css']
 })
-
 export class PersonComponent implements OnInit {
-  form: FormGroup;
-  person: object;
 
-  constructor(private data: DataService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.form = new FormGroup ({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-    });
   }
-
-  submit = function(formData) {
-    if (this.form.invalid) {
-      return;
-    }
-
-    this.person = {
-      firstName: formData.firstName,
-      lastName: formData.lastName
-    };
-
-    this.data.registerPerson(this.person)
-      .subscribe(msg => {
-        this.msg = msg.body;
-        console.log(msg);
-      });
-  };
 
 }
--- angularddd/src/app/app.module.ts
+++ angularddd/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,10 @@ import { PersonComponent } from './person/person.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
