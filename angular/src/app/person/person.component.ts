026be674bf4fa2247adb86c9e5b7e80befbc5387
AS <misstechspec@gmail.com>
2019-04-09 19:32:48 +1000

    add form intake

--- angular/src/app/person/person.component.ts
+++ angular/src/app/person/person.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.scss']
 })
-export class PersonComponent implements OnInit {
 
-  constructor() { }
+export class PersonComponent implements OnInit {
+  form: FormGroup;
+  person: object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.form = new FormGroup ({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+    });
   }
 
 }
