f68351ec31aa63ecc89ac6c4c36412c31b521847
AS <misstechspec@gmail.com>
2019-04-10 14:19:10 +1000

    add files for deployment

---
+++ server.js
@@ -0,0 +1,30 @@
+// require('./express/app');
+
+'use strict';
+
+// require packages and modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const personController = require('./controllers/person.controller');
+
+// middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// set routes to use express app
+personController(app);
+
+// server startup
+app.listen(1000, function () {
+    console.log('App listening on port 1000');
+});
+
+// export module so can use in other modules
+module.exports = app;
